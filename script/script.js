98f9bea6bede6369766d2ed056f673fd3a032914
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2024-11-13 12:25:37 +0100

    Changed some parameter names in script.js for clarity. Removed unnecessary id's in index.html/rawanda.html.

--- script/script.js
+++ script/script.js
@@ -3,36 +3,15 @@ const apiKey = config.weatherApiKey;
 
 // Array of Swedish cities
 const citiesSweden = [
-  "Borås",
-  "Gävle",
-  "Göteborg",
-  "Helsingborg",
   "Jönköping",
   "Linköping",
-  "Lund",
-  "Malmö",
   "Norrköping",
-  "Södertälje",
   "Stockholm",
-  "Umeå",
-  "Uppsala",
-  "Västerås",
-  "Örebro",
+  "Umeå"
 ];
 
 // Array of Rwandan cities
-const citiesRwanda = [
-  "Butare",
-  "Gisenyi",
-  "Huye",
-  "Kigali",
-  "Kayonza",
-  "Musanze",
-  "Nyagatare",
-  "Rubavu",
-  "Ruhengeri",
-  "Rwamagana",
-];
+const citiesRwanda = ["Butare", "Huye", "Kigali", "Kayonza", "Musanze"];
 
 // Initial weather fetch for each widget (optional, for default display)
 fetchWeather("Linköping", "Sweden");
@@ -43,7 +22,7 @@ populateDropdown("cityDropdownSweden", citiesSweden, "Sweden");
 populateDropdown("cityDropdownRwanda", citiesRwanda, "Rwanda");
 
 // Function to populate the dropdown for a specific widget
-function populateDropdown(cityDropdownId, cities, widgetId) {
+function populateDropdown(cityDropdownId, cities, country) {
   const dropdown = document.getElementById(cityDropdownId);
   dropdown.innerHTML = ""; // Clear previous options if any
   cities.forEach((city) => {
@@ -53,7 +32,7 @@ function populateDropdown(cityDropdownId, cities, widgetId) {
     cityLink.href = "#";
     cityLink.textContent = city; //Sets visible text to links name
     cityLink.onclick = () => {
-      fetchWeather(city, widgetId); //When choosing a city from the dropdown
+      fetchWeather(city, country); //When choosing a city from the dropdown
     };
     cityItem.appendChild(cityLink); //Inserts the link into the list item
     dropdown.appendChild(cityItem); //Adds the list item to the dropdown menu
@@ -61,7 +40,7 @@ function populateDropdown(cityDropdownId, cities, widgetId) {
 }
 
 // Function to fetch weather data for a specific city and update a specific widget
-function fetchWeather(city, widgetPrefix) {
+function fetchWeather(city, country) {
   fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
     .then((response) => response.json())
     .then((data) => {
@@ -73,31 +52,28 @@ function fetchWeather(city, widgetPrefix) {
       const wind = data.current.wind_kph;
       const humidity = data.current.humidity;
 
-      document.getElementById(`cityName${widgetPrefix}`).textContent = location;
+      document.getElementById(`cityName${country}`).textContent = location;
       document.getElementById(
-        `temperature${widgetPrefix}`
+        `temperature${country}`
       ).textContent = `${temperature}°C`;
-      document.getElementById(`condition${widgetPrefix}`).textContent =
-        condition;
+      document.getElementById(`condition${country}`).textContent = condition;
       document.getElementById(
-        `wind${widgetPrefix}`
+        `wind${country}`
       ).textContent = `Wind: ${wind} km/h`;
       document.getElementById(
-        `humidity${widgetPrefix}`
+        `humidity${country}`
       ).textContent = `Humidity: ${humidity}%`;
-      document.getElementById(
-        `time${widgetPrefix}`
-      ).textContent = `Local time: ${data.location.localtime.split(" ")[1]}`;
-      document.getElementById(
-        `weatherIcon${widgetPrefix}`
-      ).src = `https:${icon}`;
+      document.getElementById(`time${country}`).textContent = `Local time: ${
+        data.location.localtime.split(" ")[1]
+      }`;
+      document.getElementById(`weatherIcon${country}`).src = `https:${icon}`;
 
       // Update the time
       const currentTime = new Date().toLocaleTimeString();
-      document.getElementById(`time${widgetPrefix}`).textContent = currentTime;
+      document.getElementById(`time${country}`).textContent = currentTime;
     })
     .catch((error) => {
-      document.getElementById(`cityName${widgetPrefix}`).textContent =
+      document.getElementById(`cityName${country}`).textContent =
         "Error fetching data";
     });
 }
